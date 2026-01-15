653404030c50c178a12312998e5867741fd81428
Princy <otolugu@gmail.com>
2026-01-15 15:46:37 +0200

    feat: split Webpack config into dev/prod environments
    - Created webpack.common.js with shared configuration
    - Added webpack.dev.js for development with source maps
    - Added webpack.prod.js for production optimization
    - Updated package.json scripts: 'dev', 'build', 'watch'
    - Installed webpack-merge for config composition
    - Removed monolithic webpack.config.js
    
    Benefits:
    - Faster development builds with hot reload
    - Optimized production bundles
    - Clean separation of concerns
    - Industry-standard setup

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,15 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
+  
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",  // Make sure this matches your file
-      filename: "index.html",
-    }),
-  ],
+  
   module: {
     rules: [
       {
@@ -27,9 +20,16 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[hash][ext][query]"  // Hashed names for cache busting
+          filename: "images/[hash][ext][query]"
         }
       },
     ],
   },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",  // ← CHANGED TO template.html
+      filename: "index.html",
+    }),
+  ],
 };
